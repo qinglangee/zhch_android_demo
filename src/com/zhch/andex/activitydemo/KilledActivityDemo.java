bf7303360015bd79c0d021734675c49327a9f20c
chengz <chengz@staff.l99.com>
2015-08-18 11:16:56 +0800

    add new

--- src/com/zhch/andex/activitydemo/KilledActivityDemo.java
+++ src/com/zhch/andex/activitydemo/KilledActivityDemo.java
@@ -4,7 +4,9 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.zhch.andex.R;
@@ -13,19 +15,46 @@ import com.zhch.andex.common.activity.BaseAct;
 /**
  * Created by zhch on 15-8-12.
  */
-public class KilledActivityDemo extends BaseAct {
+public class KilledActivityDemo extends BaseAct implements View.OnClickListener{
+
+    RelativeLayout view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View view = new RelativeLayout(this);
+        view = new RelativeLayout(this);
+        view.setId(R.id.linear);
+        Button btn = new Button(this);
+        btn.setText("Press");
+        btn.setOnClickListener(this);
+        view.addView(btn);
         setContentView(view);
 
-        Fragment frag = new KilledActivityFrag();
+    }
+
+    @Override
+    public void onClick(View v) {
+
         FragmentManager manager = getFragmentManager();
+
+        Fragment frag = null;
+
+//        frag = manager.findFragmentByTag("kill_frag");
+
+        Log.d("@@@@@@@@@@@@@@@@@@@@", "Activity create  frag is null: " + (frag == null));
+
+        if(frag == null){
+
+            frag = new KilledActivityFrag();
+            Bundle args = new Bundle();
+            args.putString("arg", "from act");
+            frag.setArguments(args);
+        }
+
+
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(view.getId(), frag);
+        transaction.replace(view.getId(), frag, "kill_frag");
 
         transaction.commit();
     }
--- src/com/zhch/andex/activitydemo/KilledActivityFrag.java
+++ src/com/zhch/andex/activitydemo/KilledActivityFrag.java
@@ -1,12 +1,17 @@
 package com.zhch.andex.activitydemo;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.zhch.andex.R;
 import com.zhch.andex.common.fragment.BaseFrag;
 
@@ -21,7 +26,30 @@ public class KilledActivityFrag extends BaseFrag {
         TextView text = new TextView(getContext());
         String str = getResources().getString(R.string.app_name);
 
-        text.setText("哈哈哈哈哈哈哈哈, 我是 fragment." + str);
+        text.setText("ha ha ha, I'm fragment." + str);
+
+        Bundle arguments = getArguments();
+        final String pre;
+        if(arguments == null){
+            pre = "HAVE NOT ARGS";
+        }else {
+            String arg = arguments.getString("arg");
+            pre = "ARGS IS :" + arg;
+        }
+
+        Activity act = getActivity();
+        Log.d("@@@@@@@@@@@@@@@@@@@@", pre + " on create act is null : " + (act == null));
+
+        StringRequest request = new StringRequest("http://www.baidu.com", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Activity act = getActivity();
+                Log.d("@@@@@@@@@@@@@@@@@@@@", pre + " listener act is null : " + (act == null));
+                getActivity().getResources();
+            }
+        }, null);
+
+        Volley.newRequestQueue(getActivity().getApplicationContext()).add(request);
 
         return text;
     }
